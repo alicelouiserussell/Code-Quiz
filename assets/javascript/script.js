fe95fa5168f9e031e76792baebbb2f90e5f38f10
Alice Russell <alice.louise.russell@gmail.com>
2019-10-18 15:43:05 -0400

    scripted quis pages with js and checked user input against answer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,3 @@
-// INITIAL VARIABLES //
 var indexContent = document.querySelector("#content-area");
 var startButton = document.querySelector("#start-button");
 var timeElement = document.querySelector("#time");
@@ -34,6 +33,21 @@ var questions = [
   ];
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
 
 startButton.addEventListener("click", function() {
@@ -49,7 +63,48 @@ startButton.addEventListener("click", function() {
           clearInterval(countInterval);
         }
       }, 1000);
-});
+
+   // define new elements being created //
+    //QUESTION ELEMENTS//
+    var newQuestion = document.createElement("h2");
+    var newDiv = document.createElement("div");
+
+    //LIST ELEMENTS//
+    var ol = document.createElement("ul");
+    var li1 = document.createElement("li");
+    var li2 = document.createElement("li");
+    var li3 = document.createElement("li");
+    var li4 = document.createElement("li");
+
+    // DEFINE CONTENT //
+    ol.setAttribute("id", "list");
+    newQuestion.textContent = questions[0].title;
+    console.log(newQuestion);
+    li1.innerHTML = questions[0].choices[0];
+    li2.innerHTML = questions[0].choices[1];
+    li3.innerHTML = questions[0].choices[2];
+    li4.innerHTML = questions[0].choices[3];
+  
+    // REPLACE existing elements, APPEND to page //
+    indexContent.parentNode.replaceChild(newDiv, indexContent);
+    newDiv.appendChild(newQuestion);
+    document.body.appendChild(ol);
+    ol.appendChild(li1);
+    ol.appendChild(li2);
+    ol.appendChild(li3);
+    ol.appendChild(li4);
+
+    var correctAnswer = questions[0].answer;
+    var listElement = document.querySelector("#list"); 
+    listElement.addEventListener("click", function(event){
+      var userGuess = event.target.textContent;
+      if (correctAnswer === userGuess){
+        alert("Correct!");
+      } else {
+        alert("Incorrect!");
+        count = count - 15;
+      }
+    });
 
 
 
@@ -64,4 +119,8 @@ startButton.addEventListener("click", function() {
 
 
 
+
+
+
+    });
 });
